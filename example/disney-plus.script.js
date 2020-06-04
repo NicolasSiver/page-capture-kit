477b11c35591afd40ae14053333ab4ca44947113
Nicolas Siver <nicolas@siver.im>
2020-06-04 19:43:35 -0400

    add disney+ example

---
+++ example/disney-plus.script.js
@@ -0,0 +1,25 @@
+class DisneyPlus {
+    // Call for authentication form and login
+    step0(context) {
+        return context.series(
+            context.open('https://www.disneyplus.com/login'),
+            context.login(process.env.USERNAME_DISNEY_PLUS, process.env.PASSWORD_DISNEY_PLUS),
+            context.next()
+        );
+    }
+
+    // Capture Billing
+    step1(context) {
+        return context.series(
+            context.open('https://www.disneyplus.com/account/subscription'),
+            context.capture('Disney-Plus'),
+            context.end()
+        )
+    }
+
+    visit({step}, context) {
+        return this['step' + step](context);
+    }
+}
+
+module.exports = DisneyPlus;
