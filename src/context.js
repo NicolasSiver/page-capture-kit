45658db6d6e3a94a4480669e6261dc6d9df7655c
Nicolas Siver <nicolas@siver.im>
2020-06-03 22:11:33 -0400

    add open, series and hover actions for the context

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 const Promise = require('bluebird'),
+      format  = require('date-fns/format'),
       path    = require('path');
 
 const {Authentication} = require('./challenges/authentication'),
@@ -9,37 +10,47 @@ class Context {
         this.outputPath = outputPath;
     }
 
-    capture(fileName) {
+    capture(fileName, pattern = 'yyyy-MM-dd--HH-mm-ss') {
+        let name = `${fileName}-${format(new Date(), pattern)}.png`;
+
         return Promise
             .resolve()
             .then(() => this.page.screenshot({
                 fullPage: true,
-                path    : path.resolve(this.outputPath, fileName),
+                path    : path.resolve(this.outputPath, name),
                 type    : 'png'
             }));
     }
 
-    click(selector) {
-        let clickHandler;
-
-        if (typeof selector === 'string') {
-            if (selector.startsWith('//') === true) {
-                clickHandler = this.page.$x(selector);
-            } else {
-                clickHandler = this.page.$(selector);
-            }
-        } else {
-            clickHandler = selector;
-        }
-
+    click(selector, navigation = true) {
         return Promise.all([
-            this.page.waitForNavigation(),
-            clickHandler.click()
+            navigation === true ? this.page.waitForNavigation() : Promise.resolve(),
+            this.getElementBySelector(selector).then(element => element.click())
         ]);
     }
 
     end() {
         return ContextActions.END;
+    }
+
+    getElementBySelector(selector) {
+        let result;
+
+        if (typeof selector === 'string') {
+            if (selector.startsWith('//') === true) {
+                result = this.page.waitForXPath(selector);
+            } else {
+                result = this.page.waitForSelector(selector);
+            }
+        } else {
+            result = Promise.resolve(selector);
+        }
+
+        return result;
+    }
+
+    hover(selector) {
+        return this.getElementBySelector(selector).then(element => element.hover());
     }
 
     initWithPage(page) {
@@ -48,14 +59,33 @@ class Context {
         return this;
     }
 
-    login(username, password) {
+    login(username, password, twoStep = false) {
         return Promise
             .resolve()
-            .then(() => new Authentication(this.page).execute(username, password));
+            .then(() => {
+                return new Authentication(this.page)
+                    .setTwoStep(twoStep)
+                    .execute(username, password);
+            });
     }
 
     next() {
         return ContextActions.NEXT;
+    }
+
+    open(url) {
+        console.log('Visiting URL: ' + url);
+        return this.page.goto(url, {waitUntil: ['load', 'domcontentloaded', 'networkidle0']});
+    }
+
+    series(...actions) {
+        let chain = Promise.resolve();
+
+        actions.forEach(action => {
+            chain = chain.then(() => action);
+        });
+
+        return chain;
     }
 }
 
